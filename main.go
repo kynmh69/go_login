a19dab371699c372cc3cb0c292eace38c797b7db
Hiroki Kayanuma <hiroki.kayanuma.od@ambl.co.jp>
2022-11-04 14:09:59 +0900

    initial dev #1 create logout page.

--- main.go
+++ main.go
@@ -6,8 +6,20 @@ import (
 	"net/http"
 )
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	htmlFilePath := "./src/template/login.html"
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	htmlFilePath := "src/template/login.html"
+	log.Println("load html", htmlFilePath)
+	t, err := template.ParseFiles(htmlFilePath)
+	if err != nil {
+		log.Fatalln("error", err)
+	}
+	if err := t.Execute(w, nil); err != nil {
+		log.Fatalln("error", err)
+	}
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	htmlFilePath := "src/template/logout.html"
 	log.Println("load html", htmlFilePath)
 	t, err := template.ParseFiles(htmlFilePath)
 	if err != nil {
@@ -20,6 +32,7 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	log.Println("start serve.")
-	http.HandleFunc("/login", mainHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/logout", logoutHandler)
 	http.ListenAndServe(":8000", nil)
 }
