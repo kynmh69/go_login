2c635d30defb43b22f5d155f1ca8cf00ba9d0d45
kynmh69 <hiroki.kayanuma@gmail.com>
2022-11-04 20:23:07 +0900

    initial dev #1 mod gomod

--- web/main.go
+++ web/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
-	"go_login/web/controller"
 	"go_login/web/logging"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	file := logging.SetLogger()
 	logger := logging.GetLogger()
 	logger.LogLevel = logging.INFO
 	logger.Info("start serve.")
-	http.HandleFunc("/login", controller.LoginHandler)
-	http.HandleFunc("/logout", controller.LogoutHandler)
-	http.ListenAndServe(":8000", nil)
+
+	r := gin.Default()
+	r.LoadHTMLGlob("template/*")
+
+	r.GET("/login", func(ctx *gin.Context) {
+		ctx.HTML(
+			http.StatusOK,
+			"login.html",
+			nil,
+		)
+	})
+	r.Run(":8000")
+	// http.HandleFunc("/login", controller.LoginHandler)
+	// http.HandleFunc("/logout", controller.LogoutHandler)
+	// http.ListenAndServe(":8000", nil)
 
 	defer file.Close()
 }
