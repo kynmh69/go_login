d628bdf980897725933d5e291a982c4ce9f3b49d
kynmh69 <hiroki.kayanuma@gmail.com>
2022-11-05 10:29:52 +0900

    initial dev #1 mod router

---
+++ web/controller/router.go
@@ -0,0 +1,42 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetRouter() *gin.Engine {
+	r := gin.Default()
+	r.LoadHTMLGlob("template/*")
+
+	r.GET("/login", func(ctx *gin.Context) {
+		ctx.HTML(
+			http.StatusOK,
+			"login.html",
+			nil,
+		)
+	})
+	r.GET("/logout", func(ctx *gin.Context) {
+		ctx.HTML(
+			http.StatusOK,
+			"logout.html",
+			nil,
+		)
+	})
+	r.GET("signup", func(ctx *gin.Context) {
+		ctx.HTML(
+			http.StatusOK,
+			"siginup.html",
+			nil,
+		)
+	})
+	r.POST("signup", func(ctx *gin.Context) {
+		ctx.HTML(
+			http.StatusCreated,
+			"login.html",
+			nil,
+		)
+	})
+	return r
+}
--- web/main.go
+++ web/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
+	"go_login/controller"
 	"go_login/logging"
 	"go_login/utils"
-	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -17,24 +15,7 @@ func main() {
 
 	utils.LoadEnv()
 
-	r := gin.Default()
-	r.LoadHTMLGlob("template/*")
-
-	r.GET("/login", func(ctx *gin.Context) {
-		ctx.HTML(
-			http.StatusOK,
-			"login.html",
-			nil,
-		)
-	})
-	r.GET("/logout", func(ctx *gin.Context) {
-		ctx.HTML(
-			http.StatusOK,
-			"logout.html",
-			nil,
-		)
-	})
-
+	r := controller.GetRouter()
 	port := os.Getenv("HTTP_PORT")
 	r.Run(":" + port)
 	// http.HandleFunc("/login", controller.LoginHandler)
