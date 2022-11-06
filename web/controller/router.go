3138ae015536d520e4dadc32305299d79bd0fd18
kynmh69 <hiroki.kayanuma@gmail.com>
2022-11-06 11:58:44 +0900

    add session management #3 mod router

--- web/controller/router.go
+++ web/controller/router.go
@@ -1,6 +1,10 @@
 package controller
 
 import (
+	"go_login/model"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,11 +12,45 @@ func GetRouter() *gin.Engine {
 	r := gin.Default()
 	r.LoadHTMLGlob("template/*")
 
-	r.GET("/login", getLogin)
-	r.POST("/login", postLogin)
-	r.GET("/logout", getLogout)
-	r.GET("/signup", getSignUp)
-	r.POST("/signup", postSignUp)
-	r.GET("/home", GetHome)
+	loginCheckGroup := r.Group("/", checkLogin())
+	{
+		loginCheckGroup.GET("/home", GetHome)
+		loginCheckGroup.GET("/logout", getLogout)
+
+	}
+	logoutCheckGroup := r.Group("/", checkLogout())
+	{
+		logoutCheckGroup.GET("/login", getLogin)
+		logoutCheckGroup.POST("/login", postLogin)
+		logoutCheckGroup.GET("/signup", getSignUp)
+		logoutCheckGroup.POST("/signup", postSignUp)
+	}
+
 	return r
+}
+
+func checkLogin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cookieKey := os.Getenv("LOGIN_USER_ID_KEY")
+		id := model.GetSession(c, cookieKey)
+		if id == nil {
+			c.Redirect(http.StatusFound, "/login")
+			c.Abort()
+		} else {
+			c.Next()
+		}
+	}
+}
+
+func checkLogout() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cookieKey := os.Getenv("LOGIN_USER_ID_KEY")
+		id := model.GetSession(c, cookieKey)
+		if id != nil {
+			c.Redirect(http.StatusFound, "/")
+			c.Abort()
+		} else {
+			c.Next()
+		}
+	}
 }
